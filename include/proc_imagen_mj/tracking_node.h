8ef1ccb7433abbd40c8040d5246ad238a37fc1cf
jgines <jonathangines@hotmail.com>
2017-12-07 10:15:54 +0100

    initial commit

---
+++ include/proc_imagen_mj/tracking_node.h
@@ -0,0 +1,44 @@
+/*
+ * tracking_node.h
+ *
+ *  Created on: 05/12/2017
+ *      Author: Jonathan Ginés
+ */
+
+#ifndef TRACKINGNODE_H_
+#define TRACKINGNODE_H_
+
+#include <ros/ros.h>
+#include <string>
+#include "sensor_msgs/Image.h"
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <time.h>
+#include <sstream>
+namespace tracking_node {
+
+
+class TrackingNode {
+public:
+
+	TrackingNode();
+
+	virtual void step();
+	virtual void imgCallback(const sensor_msgs::Image::ConstPtr& img_in);
+
+private:
+	ros::NodeHandle nh_;
+	ros::NodeHandle private_nh_;
+	ros::Subscriber image_sub;
+	image_transport::ImageTransport it_;
+	image_transport::Publisher image_pub;
+	int hupper, hlower,x,y,c,supper,slower,vupper,vlower;
+
+};
+
+}
+
+#endif /* TRACKINGNODE_H_ */
---
+++ src/tracking_node.cpp
@@ -0,0 +1,103 @@
+/*
+ *
+ *
+ * Autor: Jonathan Ginés Clavero (jonathangines@hotmail.com)
+ *
+ *
+ */
+
+#include "proc_imagen_mj/tracking_node.h"
+
+namespace tracking_node {
+
+	TrackingNode::TrackingNode():
+		it_(nh_),
+  	private_nh_("~"){
+			image_sub = nh_.subscribe("/camera/rgb/image_raw", 5, &TrackingNode::imgCallback,this);
+			image_pub = it_.advertise("/image_converter/output", 1);
+			cvNamedWindow("Imagen filtrada");
+			hlower = 112;
+			hupper = 149;
+			slower = 242;
+			supper = 255;
+			vlower = 90;
+			vupper = 255;
+	}
+
+
+	void
+	TrackingNode::imgCallback(const sensor_msgs::Image::ConstPtr& img_in){
+		cv_bridge::CvImagePtr cv_ptr, cv_imageout;
+    try
+    {
+         cv_ptr = cv_bridge::toCvCopy(img_in, sensor_msgs::image_encodings::BGR8);
+         cv_imageout = cv_bridge::toCvCopy(img_in, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+         ROS_ERROR("cv_bridge exception: %s", e.what());
+         return;
+    }
+
+    cv::Mat hsv;
+    cv::cvtColor(cv_ptr->image, hsv, CV_RGB2HSV);
+
+    int height = hsv.rows;
+    int width = hsv.cols;
+    int step = hsv.step;
+    int channels = 3;
+
+    x = 0;
+    y = 0;
+    c = 0;
+
+    for(int i = 0; i <height; i++ )
+    	for(int j = 0; j <width; j++ ) {
+				int posdata = i*step+j*channels; //Only H channel
+				if(!((hsv.data[posdata] >= hlower) && (hsv.data[posdata] <= hupper) &&
+					(hsv.data[posdata+1]>=slower)&& (hsv.data[posdata+1] <= supper)&&
+					(hsv.data[posdata+2] >= vlower)&&(hsv.data[posdata+2] <= vupper))){
+						cv_imageout->image.data[posdata] = 0;
+						cv_imageout->image.data[posdata+1] = 0;
+						cv_imageout->image.data[posdata+2] = 0;
+				}else{
+					x = x + j;
+					y = y + i;
+					c++;
+				}
+			}
+			int coorX = x/c;
+			int coorY = y/c;
+			ROS_INFO("Coordenada centro pelota %d %d",coorX,coorY);
+			cv::Point p;
+			p.x = coorX;
+			p.y = coorY;
+			cv::circle(cv_imageout->image,p, 20, (0,0,255));
+			cv::imshow("Imagen filtrada", cv_imageout->image);
+			cv::waitKey(3);
+    	image_pub.publish(cv_imageout->toImageMsg());
+	}
+
+	void
+	TrackingNode::step(){
+
+	}
+}
+
+using namespace tracking_node;
+
+int
+main(int argc, char** argv)
+{
+	ros::init(argc, argv, "tracking_node");		//Inicializa el nodo
+	TrackingNode tr_node;
+	ros::Rate loop_rate(20);
+
+	/*while (ros::ok()){
+  	tr_node.step();
+		ros::spinOnce();
+		loop_rate.sleep();
+	}*/
+	ros::spin();
+	return 0;
+}
